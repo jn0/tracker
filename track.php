747dd0d84e8a96e746f96f4eb2da744ab08860dd
jno <jno@blackbox>
2018-03-15 21:04:59 +0300

    sputnik made to work

--- track.php
+++ track.php
@@ -2,6 +2,8 @@
 if (count($_REQUEST) != 1 or !isset($_REQUEST['u']))
 	header("Content-Type: text/plain; charset=UTF-8");
 
+$API_SPUTNIK = '';
+
 function d_fract($dt, $q, $sig) {
     $o = null;
     if ($dt > $q) {
@@ -23,32 +25,99 @@ function dhms($dt) {
     return implode(' ', $o);
 }
 
-function get_picture($point, $id, $zoom=13, $layer='map') {
-    $fn = "map-$id.png";
+function fetch($url) {
+    global $php_errormsg;
+
+    $data = '';
+    $fp = fopen($url, 'r');
+    if ($fp) {
+        while (!feof($fp)) { $s = fread($fp, 4096); $data .= $s; }
+        fclose($fp);
+    } else die("\n\nURL [$url] msg [$php_errormsg]\n\n");
+    return $data;
+}
+
+function save($fn, $data) {
+    switch (gettype($data)) {
+    case 'array': $data = implode("\n", $data); break;
+    }
+    $fp = fopen($fn, 'w');
+    fputs($fp, $data);
+    fclose($fp);
+}
+
+function get_picture_sputnik($point, $id, $zoom=13, $layer='map') {
+    $fn = "map-sputnik-$id.png";
+    if (!file_exists($fn)) {
+        $LL = 
+
+        $url = 'http://static-api.maps.sputnik.ru/v1/?'
+                ."z=$zoom"
+                .'&clng='.$point['lon'].'&clat='.$point['lat']
+                .'&mlng='.$point['lon'].'&mlat='.$point['lat']
+                ."&width=400&height=400"
+                .'';
+        # $url .= "&apikey=$API_SPUTNIK";
+
+        save($fn, fetch($url));
+    }
+    return $fn;
+}
+
+function get_places_sputnik($point, $id) {
+    global $API_SPUTNIK;
+
+    $fn = "map-sputnik-$id.cache";
+    if (file_exists($fn)) {
+        $pl = File($fn, FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+        return $pl;
+    }
+
+    # lat     широта     55.76228158365787
+    # lon     долгота     37.854766845703125
+    # houses  true - поиск до здания, false - до района города     true
+    # callback    имя callback-функции для запроса в форме JSONP   jsonp_123
+    # apikey  API-ключ  5032f91e8da6431d8605-f9c0c9a00357
+
+    $url = 'http://whatsthere.maps.sputnik.ru/point?'
+            .'houses=true'
+            .'&lat='.$point['lat']
+            .'&lon='.$point['lon']
+            .'';
+
+    $data = fetch($url);
+    $pl = array();
+
+    $j = json_decode($data, true);
+    # jq .result.address[].features[] < sputnik.json | jq .properties.display_name
+    foreach($j['result']['address'] as $av) {
+        foreach($av['features'] as $fv) {
+                $pl[] = $fv['properties']['display_name'];
+        }
+    }
+    save($fn, $pl);
+    return $pl;
+}
+
+function get_picture_yandex($point, $id, $zoom=13, $layer='map') {
+    $fn = "map-yandex-$id.png";
     if (!file_exists($fn)) {
         $LL = $point['lon'].','.$point['lat'];
 
         $url = 'https://static-maps.yandex.ru/1.x/?lang=ru_RU';
         $url .= "&ll=$LL";
-        $url .= "&size=450,450";
+        $url .= "&size=400,400";
         $url .= "&z=$zoom";
         $url .= "&l=$layer";
         $url .= "&pt=$LL,".'pm2'.'db'.'m'.'1';
 
-        $fp = fopen($url, 'r');
-        $data = '';
-        while (!feof($fp)) { $s = fread($fp, 4096); $data .= $s; }
-        fclose($fp);
-
-        $fp = fopen($fn, 'w');
-        fputs($fp, $data);
-        fclose($fp);
+        save($fn, fetch($url));
     }
     return $fn;
 }
 
-function get_places($point, $id) {
-    $fn = "map-$id.cache";
+function get_places_yandex($point, $id) {
+    $fn = "map-yandex-$id.cache";
     if (file_exists($fn)) {
         $pl = File($fn, FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
     } else {
@@ -84,10 +153,7 @@ function get_places($point, $id) {
 
             # echo "<br /><pre>$url</pre><br />\n";
 
-            $fp = fopen($url, 'r');
-            $data = '';
-            while (!feof($fp)) { $s = fread($fp, 4096); $data .= $s; }
-            fclose($fp);
+            $data = fetch($url);
 
             $j = json_decode($data, true);
             # jq .response.GeoObjectCollection.featureMember[] < "$1" |
@@ -109,10 +175,7 @@ function get_places($point, $id) {
             $pl[] = $d['text'];
         }
  
-        $fp = fopen($fn, 'w');
-        fputs($fp, implode("\n", $pl));
-        fclose($fp);
-
+        save($fn, $pl);
     }
     return $pl;
 }
@@ -153,7 +216,7 @@ function show_request() {
             continue;
         }
 
-        if ($matched) {
+        if ($matched and $s) {
             list($k, $v) = explode('=', $s, 2);
             if (array_key_exists($k, $point)) {
                 $point[$k] = $v;
@@ -171,25 +234,32 @@ function show_request() {
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <style>
   div#top { max-width: 800px; margin-left: auto; margin-right: auto; }
-  div#map { width: 450px; height: 450px; float: left; }
-  div#places { float: left; width: 350px; }
+  div.map { width: 400px; height: 400px; float: left; }
+  div.places { float: left; width: 400px; }
   ul#args>li { display: inline-block; width: 32ex; }
+  hr { clear: both }
  </style>
 </head>
 <body>
 <div id=top>
 <?php
     echo "<ul id=args><li>point: $cnt</li>\n<li>back: $dt</li>\n";
-    foreach($last as $k=>$v) {
-        echo "<li>$k: $v</li>\n";
-    }
+    foreach($last as $k=>$v) { echo "<li>$k: $v</li>\n"; }
     echo "</ul>\n<hr />\n";
-    $fn = get_picture($last, $cnt);
+
+    $fn = get_picture_yandex($last, $cnt);
     $url = 'https://yandex.ru/maps/?mode=search&text='.$last['lat'].','.$last['lon'];
-    echo "<div id=map><a href=\"$url\"><img src=\"$fn\" alt=\"[map]\" /></a></div>\n";
-    $pl = get_places($last, $cnt);
+    echo "<div class=map><a href=\"$url\"><img src=\"$fn\" alt=\"[yandex map]\" /></a></div>\n";
+
+    $fn = get_picture_sputnik($last, $cnt);
+    $url = 'https://maps.sputnik.ru/?lat='.$last['lat'].'&lon='.$last['lon'];
+    echo "<div class=map><a href=\"$url\"><img src=\"$fn\" alt=\"[sputnik map]\" /></a></div>\n";
+
+    echo "<hr />\n";
+
+    $pl = get_places_yandex($last, $cnt);
     reset($pl);
-    echo "<div id=places><ul>\n";
+    echo "<div class=places><ul>\n";
     $LL = $last['lon'].','.$last['lat'];
     for ($i = 0; $i < count($pl); $i++) {
         $url = "https://yandex.ru/maps/213/moscow/?mode=search"
@@ -197,6 +267,20 @@ function show_request() {
         echo "<li><a href=\"$url\">".$pl[$i]."</a></li>\n";
     }
     echo "</ul></div>\n";
+
+    $pl = get_places_sputnik($last, $cnt);
+    reset($pl);
+    echo "<div class=places><ul>\n";
+    $LL = $last['lon'].','.$last['lat'];
+    for ($i = 0; $i < count($pl); $i++) {
+        $url = "https://maps.sputnik.ru/?zoom=14"
+            .'&lng='.$last['lon']
+            .'&lat='.$last['lat']
+            ."&q=".urlencode($pl[$i]);
+        echo "<li><a href=\"$url\">".$pl[$i]."</a></li>\n";
+    }
+    echo "</ul></div>\n";
+
 ?></div></body></html>
 <?php
 }
