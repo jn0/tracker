0b297ca4d0a9f5a633900b720720f7003e841407
jno <jno@blackbox>
2018-04-03 21:01:34 +0300

    add autorefresh

--- track.php
+++ track.php
@@ -269,6 +269,7 @@ function show_request() {
 <html>
 <head>
  <meta charset="utf-8">
+ <meta http-equiv="refresh" content="35">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title><?php echo("$cnt".($u ? " ($u)" : '')) ?></title>
  <link rel="stylesheet" href="bootstrap.min.css">
